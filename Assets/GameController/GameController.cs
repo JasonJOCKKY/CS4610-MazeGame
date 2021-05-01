faf7e5c75bc52be10f6662bec36603d84814a5f7
Jingsong Tan <jasontanjingsong@gmail.com>
2021-05-01 17:37:40 -0500

    maze update, minimap started

--- Assets/GameController/GameController.cs
+++ Assets/GameController/GameController.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+  public Camera miniMapCamera;
   private CreateMap _mazeCreator;
   // Start is called before the first frame update
   void Start()
   {
     _mazeCreator = GetComponent<CreateMap>();
     _mazeCreator.Creat();
+    miniMapCamera.gameObject.transform.position = new Vector3(_mazeCreator.X / 2 - 0.5f, 40, _mazeCreator.Y / 2 - 0.5f);
+    miniMapCamera.orthographicSize = _mazeCreator.X / 2;
   }
 
   // Update is called once per frame
